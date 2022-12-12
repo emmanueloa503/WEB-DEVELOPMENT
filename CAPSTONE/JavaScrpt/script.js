0acf1ec34ded14e958d2ecc93c7f0547c803761e
Emmanuel Owusu-Ampaw <118029142+emmanueloa503@users.noreply.github.com>
2022-12-12 00:12:28 -0500

    Add files via upload

---
+++ CAPSTONE/JavaScrpt/script.js
@@ -0,0 +1,8 @@
+	//instagram API
+	var userFeed = new Instafeed({
+		get: 'user',
+		target: "instafeed-container",
+    	resolution: 'high_resolution',
+		accessToken: 'IGQVJVTFhUWFFZAUHpGQlFZAOE9odm83ZAVV2VmZAkZAE0zTk05WGRWUWtFZA1hqVnA0blJVMzdKd3FGSmJmWjJySnU2b094Ql9CUEMxdTNlVXg4aExDQ0JxOXlnb084T1VSUV9RZA0Rqd21DQjZAMbjZAzdXBkZAgZDZD'
+	});
+	userFeed.run();
